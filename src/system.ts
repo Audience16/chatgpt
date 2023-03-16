557809c99431ccd14a16c0aece5d509077fdc5be
Audience16 <127910594+Audience16@users.noreply.github.com>
2023-03-16 10:01:43 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -3,14 +3,14 @@ export const setting = {
   archiveSession: false,
   openaiAPIKey: "",
   openaiAPITemperature: 60,
-  password: "",
+  password: "A",
   systemRule: ""
 }
 
 export const message = `- 部署自 [@ourongxing](https://github.com/ourongxing) 
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击名称滚动到顶部，点击输入框滚动到底部。
 
-- 祝你开心`
+- 祝你开心！`
 
 export type Setting = typeof setting
 
