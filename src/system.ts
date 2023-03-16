f470ec7e96bdf0af6ff5d5d4b5145c9ff7804775
Audience16 <127910594+Audience16@users.noreply.github.com>
2023-03-16 10:02:10 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -8,6 +8,7 @@ export const setting = {
 }
 
 export const message = `- 部署自 [@ourongxing](https://github.com/ourongxing) 
+
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击名称滚动到顶部，点击输入框滚动到底部。
 
 - 祝你开心！`
