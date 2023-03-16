b2c394450f7f3a0b5e6e360f5af05d9ea6074086
Audience16 <127910594+Audience16@users.noreply.github.com>
2023-03-16 11:02:42 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -11,7 +11,7 @@ export const message = `- 修改自 [@ourongxing](https://github.com/ourongxing)
 
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击名称滚动到顶部，点击输入框滚动到底部。
 
-- 祝我们平日快乐`
+- 祝你开心`
 
 export type Setting = typeof setting
 
